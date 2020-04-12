6d5918e18224477d8fe04556eb91bd0412478117
Joe A <josephanson@hotmail.co.uk>
2020-04-12 03:39:07 +0100

    style: adjust editor style

---
+++ cypress/generate/createImages.spec.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+/// <reference types="cypress" />
+
+/*
+ * uses Cypress to load up and create images of all the components
+ */
+
+describe('Create Component Images', () => {
+  beforeEach(() => {
+    cy.viewport(1920, 1080);
+  });
+
+  it('Create Images', () => {
+    cy.visit('/html-blocks/all');
+
+    cy.get('.example-section').each(($el) => {
+      const content = $el.find('.example-component')[0].firstChild;
+
+      const title = $el
+        .find('.title a')
+        .text()
+        .toLowerCase()
+        .replace(/\s+/g, '-') // Replace spaces with -
+        .replace(/[^\w-]+/g, '') // Remove all non-word chars
+        .replace(/--+/g, '-') // Replace multiple - with single -
+        .replace(/^-+/, '') // Trim - from start of text
+        .replace(/-+$/, ''); // Trim - from end of text
+
+      cy.get(content).screenshot('./' + title);
+    });
+  });
+});
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,30 +1,17 @@
-const wp = require('@cypress/webpack-preprocessor');
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
 
-module.exports = (on) => {
-  const options = {
-    webpackOptions: {
-      mode: 'development',
-      resolve: {
-        extensions: ['.ts', '.js']
-      },
-      module: {
-        rules: [
-          {
-            test: /\.ts$/,
-            exclude: [/node_modules/],
-            use: [
-              {
-                loader: 'ts-loader',
-                options: {
-                  // skip typechecking for speed
-                  transpileOnly: true
-                }
-              }
-            ]
-          }
-        ]
-      }
-    }
-  };
-  on('file:preprocessor', wp(options));
-};
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+module.exports = (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+}
